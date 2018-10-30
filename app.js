6da0ce0e9df43a04ef3c74d69719d1854ec5238b
Jake Huffman <jakehuffman@gmail.com>
2018-10-29 22:54:17 -0400

    added result formatting and cli arguments

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const request = require('request');
 const conf = require('./config/secrets.js');
 
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to get weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+var encodedAddress = encodeURIComponent(argv.a);
+
 request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?key=${conf.apikey}&address=14041%20Worth%20Ave%20woodbridge`,
+  url: `https://maps.googleapis.com/maps/api/geocode/json?key=${conf.apikey}&address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
-  //console.log(JSON.stringify(response, undefined, 2));
-  // console.log(JSON.stringify(body, undefined, 2));
-  console.log(`address: ${body.results[0].formatted_address}`);
-  console.log(`lat: ${body.results[0].geometry.location.lat}`);
-  console.log(`lng: ${body.results[0].geometry.location.lng}`);
+  if (error) {
+    console.log('Unable to connect');
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('no results');
+  } else if (body.status === 'OK') {
+    console.log(`address: ${body.results[0].formatted_address}`);
+    console.log(`lat: ${body.results[0].geometry.location.lat}`);
+    console.log(`lng: ${body.results[0].geometry.location.lng}`);
+  }
 });
