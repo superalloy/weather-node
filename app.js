3a2a11084fb5fe54eacc7717ad6ca1ccf04137cf
Jake Huffman <jakehuffman@gmail.com>
2018-10-29 22:23:20 -0400

    added location info

--- app.js
+++ app.js
@@ -5,5 +5,9 @@ request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?key=${conf.apikey}&address=14041%20Worth%20Ave%20woodbridge`,
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
+  //console.log(JSON.stringify(response, undefined, 2));
+  // console.log(JSON.stringify(body, undefined, 2));
+  console.log(`address: ${body.results[0].formatted_address}`);
+  console.log(`lat: ${body.results[0].geometry.location.lat}`);
+  console.log(`lng: ${body.results[0].geometry.location.lng}`);
 });
