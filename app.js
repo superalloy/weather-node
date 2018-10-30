fbee5b7314e919f20f085573fd0350466f650221
Jake Huffman <jakehuffman@gmail.com>
2018-10-29 22:13:24 -0400

    display result

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const request = require('request');
+const conf = require('./config/secrets.js');
 
 request({
-  url: "",
+  url: `https://maps.googleapis.com/maps/api/geocode/json?key=${conf.apikey}&address=14041%20Worth%20Ave%20woodbridge`,
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  console.log(JSON.stringify(body, undefined, 2));
 });
